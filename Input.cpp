7c9303f98dba96d5a781d5f62c88d34774652507
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 10:01:05 -0500

    Declaring a string variable and store the user's name in it

---
+++ Input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string name;//declaring the string variable "name" which will will store the user's name
+	cout<<"Enter your name "<<flush;
+
+	cin>>name; //CIN stands for "Character input"
+
+	return 0;
+}
