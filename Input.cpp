d7b616d00c66bdf883da5e1d57fecef7e6d10197
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 20:07:39 -0500

    Flushing the buffer and making the user to press Enter before ending the program

--- Input.cpp
+++ Input.cpp
@@ -2,16 +2,28 @@
 using namespace std;
 
 int main(){
-	string name;//declaring the string variable "name" which will will store the user's name
-	cout<<"Enter your name "<<flush;
+        string name;//declaring the string variable "name" which will will store the user's name
+        cout<<"Enter your name "<<flush;
 
-	cin>>name; //CIN stands for "Character input"
-	cout<<"Hello "<<name<<endl;
+        cin>>name; //CIN stands for "Character input"
+        cout<<"Hello "<<name<<endl;
 
 
-	int number;//declaring an int variable
-	cout<<"Enter a number: ";	//asking for the user to enter a number
-	cin>>number;
-	cout<<"The number entered is "<<number<<endl;
-	return 0;
+        int number;//declaring an int variable
+        cout<<"Enter a number: ";       //asking for the user to enter a number
+        cin>>number;
+        cout<<"The number entered is "<<number<<endl;
+	
+	//flushing the buffer
+        int c;
+        do{
+        c=cin.get();
+        } while (c!='\n'&&c!=EOF);
+
+
+        //waiting for the user to press ENTER
+        cin.get();
+
+        return 0;
 }
+
